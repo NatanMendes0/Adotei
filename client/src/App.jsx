515a109a1db01c59f1d8592327c34cac458ecad5
Andrey Fialho <andrey@tiquetaque.com>
2025-04-02 14:22:49 -0300

    feat: Adicionar componente ScrollToTop ao App
    - Inclui o componente ScrollToTop para melhorar a navegação, garantindo que a página retorne ao topo ao mudar de rota.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
+import ScrollToTop from "./components/ScrollToTop";
 import { FavoritesProvider } from "./contexts/FavoritesContext";
 import FavoritesPage from "./pages/FavoritesPage";
 import HomePage from "./pages/HomePage";
@@ -9,6 +10,7 @@ import PetsPage from "./pages/PetsPage";
 function App() {
   return (
     <FavoritesProvider>
+      <ScrollToTop />
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<HomePage />} />
